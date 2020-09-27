a0b9e13e0527415c339847cbd8cedb73c1ba3009
Kevin Ravasco <54494051+Kevin-Ravasco@users.noreply.github.com>
2020-09-27 11:59:27 +0300

    Update settings.py

--- meranda/settings.py
+++ meranda/settings.py
@@ -131,7 +131,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'youngbossravasco@gmail.com'
-EMAIL_HOST_PASSWORD = '072681Ke'
+EMAIL_HOST_USER = 'yyouremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'youremailpassword'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
